d848247ad8dd9dfd34e15f17cf10aff90b4f9bc4
Manuel A. Bolvivar <ma.bolivar643@gmail.com>
2015-10-11 21:17:52 -0500

    Read JSON files
    Basic script to read JSON files

--- R/readYelpJSONdata.R
+++ R/readYelpJSONdata.R
@@ -2,17 +2,25 @@
 #Set working directory
 #==========================================================
 
-setd("C:/Users/ma.bolivar643/Dropbox/3. DSS/DataScience_Capstone_Project/data")
+setwd("C:/Users/ma.bolivar643/Dropbox/3. DSS/DataScience_Capstone_Project/data")
+setwd("C:/Users/Manuel/Dropbox/3. DSS/DataScience_Capstone_Project/data")
 
 #==========================================================
 #Packages
 #==========================================================
 
 library(jsonlite)
+library(dplyr)
 
 #==========================================================
 #Reading the data
 #==========================================================
 
+dir("./raw_yelp")
+f1 <- ("./raw_yelp/yelp_academic_dataset_checkin.json")
+f2 <- ("./raw_yelp/yelp_academic_dataset_review.json")
 
+dat <- fromJSON(sprintf("[%s]", paste(readLines(f2,n = 100), collapse=",")))
+str(dat)
 
+?file
